9d2ed2a4b2419a3d7649556faa227a34cfa823b3
Fabiano Lima <fabianosl@ufrrj.br>
2024-10-04 01:11:35 -0300

    feat(restaurant): adicionando o caso de uso excluir um restaurante

---
+++ src/core/destroyRestaurant.ts
@@ -0,0 +1,19 @@
+import type { Restaurant } from "./entities/Restaurant";
+import type { RestaurantRepository } from "./repositories/RestaurantRepository";
+
+export class DestroyRestaurant {
+	readonly restaurantRepository: RestaurantRepository;
+
+	constructor(restaurantRepository: RestaurantRepository) {
+		this.restaurantRepository = restaurantRepository;
+	}
+
+	async execute(id: string): Promise<Restaurant> {
+		const restaurant = await this.restaurantRepository.get(id);
+
+		await this.restaurantRepository.delete(restaurant);
+
+		return restaurant;
+	}
+    
+}
