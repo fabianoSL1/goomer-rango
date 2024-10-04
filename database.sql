ead34bd6f0ba993be4b3076ade572599a4a1e0a2
Fabiano Lima <fabianosl@ufrrj.br>
2024-10-04 03:17:48 -0300

    build(database): sql dump e ajustando docker compose

---
+++ database.sql
@@ -0,0 +1,18 @@
+CREATE TABLE restaurants (
+     id SERIAL PRIMARY KEY,
+     restaurant_name VARCHAR(255) NOT NULL,
+     address VARCHAR(255) NOT NULL,
+     picture VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE restaurant_schedules (
+    id SERIAL PRIMARY KEY,
+    restaurant_id INT NOT NULL,
+    week_day VARCHAR(15) NOT NULL,
+    start_at VARCHAR(5),
+    end_at VARCHAR(5),
+
+    FOREIGN KEY (restaurant_id) REFERENCES restaurants (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
